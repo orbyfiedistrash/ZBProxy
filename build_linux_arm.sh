e37369d2e88814840cd2228763f7d594d4b4a917
orbyfied <atomf88zero@gmail.com>
2025-03-28 20:37:55 +0100

    dwqdqwd

--- build_linux_arm.sh
+++ build_linux_arm.sh
@@ -6,6 +6,6 @@ export GOARM=7
 export CGO_ENABLED=1
 export BUILD_NAME=zbproxy
 
-echo [+++] Config: OS($GOOS) ARCH($GOARCH) ARM($GOARM) CGO($CGO_ENABLED) NAME($BUILD_NAME)
+echo "[+++] Config: OS($GOOS) ARCH($GOARCH) ARM($GOARM) CGO($CGO_ENABLED) NAME($BUILD_NAME)"
 
 go build -v -trimpath -ldflags '-s -w -buildid=' -o $BUILD_NAME ./cmd/zbproxy
